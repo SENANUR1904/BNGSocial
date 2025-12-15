3a005fb8be8622f37e1772bbd0d7d23b0fab2434
kadirSSL <kadirssl815@gmail.com>
2025-12-15 16:24:25 +0300

    bozdum sanırım

--- src/main/java/org/example/bngsocial/Main.java
+++ src/main/java/org/example/bngsocial/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+//kadir
     @Override
     public void start(Stage primaryStage) {
         try {
